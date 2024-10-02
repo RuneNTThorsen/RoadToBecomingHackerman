d75758c6c9ceea510fe48e4c2c3295a9ba89cf72
Ion <runentt@hotmail.com>
2024-10-02 23:15:00 +0200

    Solved some more of Natas

---
+++ OverTheWire/Natas/natas11_1.php
@@ -0,0 +1,18 @@
+<?php
+$cookieData = base64_decode('HmYkBwozJw4WNyAAFyB1VUcqOE1JZjUIBis7ABdmbU1GIjEJAyIxTRg%3D');
+
+function xor_encrypt($in) {
+    $key = json_encode(array( "showpassword"=>"no", "bgcolor"=>"#ffffff"));
+    $text = $in;
+    $outText = '';
+
+    // Iterate through each character
+    for($i=0;$i<strlen($text);$i++) {
+    $outText .= $text[$i] ^ $key[$i % strlen($key)];
+    }
+
+    return $outText;
+}
+
+print xor_encrypt($cookieData);
+?>
