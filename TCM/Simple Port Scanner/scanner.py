72977de0ac4774da74113ac7d3bd130c801e94de
ion <runentt@hotmail.com>
2024-10-17 15:59:10 +0200

    Added concurrency to port scanner

--- TCM/Simple Port Scanner/scanner.py
+++ TCM/Simple Port Scanner/scanner.py
@@ -3,6 +3,7 @@
 import sys
 import socket
 from datetime import datetime
+from concurrent.futures import ThreadPoolExecutor, as_completed, CancelledError
 
 
 if len(sys.argv) == 2:
@@ -12,26 +13,44 @@ else:
     print("Syntax: python scanner.py <IP-address>")
     sys.exit()
 
+now: datetime = datetime.now()
+current_time: str = now.strftime("%H:%M:%S")
+
 print("-" * 50)
 print("Scanning target " + target)
-print("Time started: " + str(datetime.now))
+print("Time started: " + current_time)
 print("-" * 50)
 
+
+def test_port(target: str, port: int):
+    s: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socket.setdefaulttimeout(1)
+    result: int = s.connect_ex((target, port))
+    if result == 0:
+        print("Port {} is open".format(port))
+    s.close()
+
+
+lower_bound: int = 50
+upper_bound: int = 85
+
+results: list = []
+
 try:
-    for port in range(50,85):
-        s: socket.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socket.setdefaulttimeout(1)
-        result: int = s.connect_ex((target, port))
-        print("Checking port {}".format(port))
-        if result == 0:
-            print("Port {} is open".format(port))
-        s.close()
-except KeyBoardInterrupt:
+    with ThreadPoolExecutor() as executor:
+        futures: dict = {executor.submit(test_port, target, i): i for i in range(lower_bound, upper_bound)}
+        print("\nFinding and printing open ports.\n\n")
+        for future in as_completed(futures):
+            try:
+                future.result()
+            except socket.gaierror:
+                print("Hostname could not be resolved.")
+                sys.exit()
+            except socket.error:
+                print("Could not connect to server.")
+                sys.exit()
+            except CancelledError:
+                print("A scan of one of the ports was cancelled")
+except KeyboardInterrupt:
     print("\nExiting program.")
-    sys.exit()
-except socket.gaierror:
-    print("Hostname could not be resolved.")
-    sys.exit()
-except socket.error:
-    print("Could not connect to server.")
     sys.exit()
